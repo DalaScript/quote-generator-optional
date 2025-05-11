adf71689a01e3f5096e70c2e2c235d1df9844a46
DalaScript <nik.dalalishvili@gmail.com>
2025-05-11 13:44:46 +0400

    added loader

--- script.js
+++ script.js
@@ -3,24 +3,31 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    if(!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
 
 // Get Quotes From API
 async function getQuote() {
+    loading();
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
     const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
-
     try {
         const response = await fetch(proxyUrl + apiUrl);
-        const text = await response.text();
-        let data;
+        const data = await response.json();
         // If Author is blank, add 'Unknown'
-        try {
-            data = JSON.parse(text);
-        }catch (jsonErr) {
-            console.error('Invalid JSON:', text);
-            return;
-        }
-
         if(data.quoteAuthor === '') {
             authorText.innerText = 'Unknown';
         }else {
@@ -33,6 +40,8 @@ async function getQuote() {
             quoteText.classList.remove('long-quote');
         }
         quoteText.innerText = data.quoteText;
+        // Stop Loader, Show Quote
+        complete();
     } catch (error) {
         getQuote();
         console.log('whoops, no quote', error);
